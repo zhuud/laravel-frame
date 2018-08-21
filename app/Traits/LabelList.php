db0966ddae2567ca108c1e72b4d02777118dbd19
zhuud <daydifly@gmail.com>
2018-08-21 15:29:46 +0800

    select

--- app/Traits/LabelList.php
+++ app/Traits/LabelList.php
@@ -4,5 +4,29 @@ namespace App\Http\Traits;
 
 trait LabelList
 {
-
+    /**
+     * select组件下拉数据
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string $id
+     * @param string $text
+     * @param bool|array $where
+     * @return array
+     */
+    protected function labelList($model, string $id, string $text, $where = false)
+    {
+        return
+            $model->when($where, function ($query, $where) {
+                return $query->where($where);
+            })
+            ->orderBy("$id", 'dsc')
+            ->get(["$id","$text"])
+            ->map(function ($item) use ($id, $text) {
+                return  [
+                    'label' => $item->$text,
+                    'value' => $item->$id,
+                ];
+            })
+            ->all();
+    }
 }
--- config/database.php
+++ config/database.php
@@ -112,7 +112,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_INDEX', 0),
+            'database' => env('REDIS_INDEX_DEFAULT', 0),
         ],
         'session' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
